3e764b1875f55817cad51780d83242de2e012a26
Anna Kovalchuk <anutamig@mail.ru>
2020-01-30 19:22:07 +0300

    Реализации умного логина и логаута, который умеет обрабатывать сбои и работать со следующими тестами

--- test/test_add_group.py
+++ test/test_add_group.py
@@ -3,7 +3,9 @@ from model.group import Group
 
 
 def test_add_group(app):
-  app.group.create(Group(name="dfgdfg", header="dfgdfgghjghj", footer="dfgdfggdfgdfgghjghjghj"))
+    app.group.create(Group(name="dfgdfg", header="dfgdfgghjghj", footer="dfgdfggdfgdfgghjghjghj"))
+    # для проверки умного логина и логаута
+    # app.session.logout()
 
 def test_add_empty_group(app):
- app.group.create(Group(name="", header="", footer=""))
+    app.group.create(Group(name="", header="", footer=""))
