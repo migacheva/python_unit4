b0294d8cb8959048fa2d240cc6b521b6ce3b0fae
Anna Kovalchuk <anutamig@mail.ru>
2020-01-30 18:57:11 +0300

    Создание интеллектуальной фикстуры, перенос вызова логина и логаута в фикстуру

--- test/test_add_contact.py
+++ test/test_add_contact.py
@@ -2,6 +2,6 @@
 from model.contact import Contact
 
 def test_add_contact(app):
-  app.session.login(username="admin", password="secret")
   app.contact.create(Contact(firstname="Атабек", lastname="Бердыбердыев", nickname="АРБУЗ"))
-  app.session.logout()
+  app.contact.create(Contact(firstname="Атабек2", lastname="Бердыбердыев2", nickname="АРБУЗ2"))
+  app.contact.create(Contact(firstname="Атабек333", lastname="Бердыбердыев333", nickname="АРБУЗ33"))
--- test/test_mod_contact.py
+++ test/test_mod_contact.py
@@ -2,6 +2,4 @@
 from model.contact import Contact
 
 def test_mod_contact(app):
-  app.session.login(username="admin", password="secret")
   app.contact.modify_first(Contact(firstname="Иван", lastname="Иванов", nickname="ПОМИДОР"))
-  app.session.logout()
