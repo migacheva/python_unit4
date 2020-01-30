c665b894c5045e11dd7e9d2f5c37cdaee2f22b49
Anna Kovalchuk <anutamig@mail.ru>
2020-01-30 18:38:54 +0300

    Добавление структуры если if-then-else в структуру теста

--- test/test_mod_group.py
+++
@@ -1,6 +0,0 @@
-from model.group import Group
-
-def test_mod_first_group(app):
-  app.session.login(username="admin", password="secret")
-  app.group.mod_first_group(Group(name="1", header="1", footer="1"))
-  app.session.logout()
