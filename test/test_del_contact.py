f7d35541bb7fdc714a9346f8b0c57d1241c3f608
Anna Kovalchuk <anutamig@mail.ru>
2020-01-31 17:42:24 +0300

    Работа с удалением контакта.

--- test/test_del_contact.py
+++ test/test_del_contact.py
@@ -1,2 +1,6 @@
+from model.contact import Contact
+
 def test_delete_first_contact(app):
-  app.contact.delete_first_contact
+  if app.contact.count() == 0:
+    app.contact.create(Contact(firstname="testtesttesttest"))
+  app.contact.delete_first_contact()
