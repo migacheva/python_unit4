c279b7e65bef683d7b27347dfe96aca5992cf086
Anna Kovalchuk <anutamig@mail.ru>
2020-02-11 19:35:41 +0300

    5 занятие 1 видос все до 10:27

--- fixture/contact.py
+++ fixture/contact.py
@@ -1,11 +1,8 @@
+from model.contact import Contact
 
 class ContactHelper:
     def __init__(self, app):
         self.app = app
-
-    def open_create_contact_page(self):
-        wd = self.app.wd
-        wd.find_element_by_link_text("add new").click()
 
     def change_field_value(self, field_name, text):
         wd = self.app.wd
@@ -14,53 +11,103 @@ class ContactHelper:
             wd.find_element_by_name(field_name).clear()
             wd.find_element_by_name(field_name).send_keys(text)
 
-    def fill_contact_form(self, contact):
-        wd = self.app.wd
-        self.change_field_value("firstname", contact.firstname)
-        self.change_field_value("lastname", contact.lastname)
-        self.change_field_value("nickname", contact.nickname)
+    contact_cache = None
 
-    def create(self, contact):
-        wd = self.app.wd
-        wd.find_element_by_link_text("add new").click()
-        # fill group firm
-        self.fill_contact_form(contact)
-        # submit group creation
-        wd.find_element_by_name("submit").click()
-        # self.return_to_groups_page()
+    def get_contact_list(self):
+        if self.contact_cache is None:
+            wd = self.app.wd
+            self.app.open_home_page()
+            self.contact_cache = []
+            for row in wd.find_elements_by_name("entry"):
+                cells = row.find_elements_by_name("td")
+                firstname = cells[1].text
+                lastname = cells[2].text
+                id = cells[0].find_element_by_tag_name("input").get_attribute("value")
+                all_phones = cells[5].text.splitlines()
+                self.contact_cache.append(Contact(firstname=firstname, lastname=lastname, id=id,
+                                                  homephone=all_phones[0], mobilephone=all_phones[1],
+                                                  workphone=all_phones[2], secondaryphone=all_phones[3]))
+        return list(self.contact_cache)
 
-    def select_first_contact(self):
+    def open_contact_to_edit_by_index(self, index):
         wd = self.app.wd
-        # Выбрать элемент для изменения (1й) selected[]
-        wd.find_element_by_name("selected[]").click()
+        self.app.open_home_page()
+        row = wd.find_elements_by_name("entry")[index]
+        cell = row.find_elements_by_tag_name("td")[7]
+        cell.find_element_by_tag_name("a").click()
 
-    def delete_first_contact(self):
+    def open_contact_view_by_index(self, index):
         wd = self.app.wd
-        # select first group
-        wd.find_element_by_name("selected[]").click()
-        # submit deletion
-# Не удается выбрать кнопку удаления
-        wd.find_element_by_css_selector("div:nth-child(8) > input[type=button]").click()
-        # Нажать ок на всплывающем окне
-        # 19 | assertConfirmation | Delete 1 addresses? |  |
-        obj = wd.switch_to.alert
-        obj.accept()
+        self.app.open_home_page()
+        row = wd.find_elements_by_name("entry")[index]
+        cell = row.find_elements_by_tag_name("td")[6]
+        cell.find_element_by_tag_name("a").click()
 
-    def modify_first(self, new_contact_data):
+    def get_contact_info_from_edit_page(self, index):
         wd = self.app.wd
-        self.select_first_contact()
-        # Нажать на карандаш title Edit
-        wd.find_element_by_css_selector("tbody tr:nth-child(2) td:nth-child(8) a img").click()
-        # Изменить имя, фамилию, никнейм
-        self.fill_contact_form(new_contact_data)
-        # Нажать обновить name="update"
-        wd.find_element_by_name("update").click()
-        self.return_to_home_page()
+        self.open_contact_to_edit_by_index(index)
+        firstname = wd.find_element_by_name("firstname").get_attribute("value")
+        lastname = wd.find_element_by_name("lastname").get_attribute("value")
+        id = wd.find_element_by_name("id").get_attribute("value")
+        homephone = wd.find_element_by_name("home").get_attribute("value")
+        workphone = wd.find_element_by_name("work").get_attribute("value")
+        mobilephone = wd.find_element_by_name("mobile").get_attribute("value")
+        secondaryphone = wd.find_element_by_name("phone2").get_attribute("value")
+        return Contact(firstname=firstname, lastname=lastname,
+                       id=id, homephone=homephone, mobilephone=mobilephone,
+                       workphone=workphone, secondaryphone=secondaryphone)
 
-    def return_to_home_page(self):
-        wd = self.app.wd
-        wd.find_element_by_link_text("home").click()
 
-    def count(self):
-        wd = self.app.wd
-        return len(wd.find_elements_by_name("selected[]"))
+#     def open_create_contact_page(self):
+#         wd = self.app.wd
+#         wd.find_element_by_link_text("add new").click()
+#     def fill_contact_form(self, contact):
+#         wd = self.app.wd
+#         self.change_field_value("firstname", contact.firstname)
+#         self.change_field_value("lastname", contact.lastname)
+#         self.change_field_value("nickname", contact.nickname)
+#
+#     def create(self, contact):
+#         wd = self.app.wd
+#         wd.find_element_by_link_text("add new").click()
+#         # fill group firm
+#         self.fill_contact_form(contact)
+#         # submit group creation
+#         wd.find_element_by_name("submit").click()
+#         # self.return_to_groups_page()
+#
+#     def select_first_contact(self):
+#         wd = self.app.wd
+#         # Выбрать элемент для изменения (1й) selected[]
+#         wd.find_element_by_name("selected[]").click()
+#
+#     def delete_first_contact(self):
+#         wd = self.app.wd
+#         # select first group
+#         wd.find_element_by_name("selected[]").click()
+#         # submit deletion
+# # Не удается выбрать кнопку удаления
+#         wd.find_element_by_css_selector("div:nth-child(8) > input[type=button]").click()
+#         # Нажать ок на всплывающем окне
+#         # 19 | assertConfirmation | Delete 1 addresses? |  |
+#         obj = wd.switch_to.alert
+#         obj.accept()
+#
+#     def modify_first(self, new_contact_data):
+#         wd = self.app.wd
+#         self.select_first_contact()
+#         # Нажать на карандаш title Edit
+#         wd.find_element_by_css_selector("tbody tr:nth-child(2) td:nth-child(8) a img").click()
+#         # Изменить имя, фамилию, никнейм
+#         self.fill_contact_form(new_contact_data)
+#         # Нажать обновить name="update"
+#         wd.find_element_by_name("update").click()
+#         self.return_to_home_page()
+#
+#     def return_to_home_page(self):
+#         wd = self.app.wd
+#         wd.find_element_by_link_text("home").click()
+#
+#     def count(self):
+#         wd = self.app.wd
+#         return len(wd.find_elements_by_name("selected[]"))
