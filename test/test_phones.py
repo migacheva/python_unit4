6a44335af3161a94b7b000c04b11a5a3e9159a0b
Anna Kovalchuk <anutamig@mail.ru>
2020-02-14 19:03:29 +0300

    Видос 5.3

--- test/test_phones.py
+++ test/test_phones.py
@@ -1,14 +1,13 @@
 import re
 from model.contact import Contact
 
+
 # Тест валится, если есть хоть один контакт, который был создан без телефонов
 def test_phones_on_home_page(app):
     contact_from_home_page = app.contact.get_contact_list()[0]
     contact_from_edit_page = app.contact.get_contact_info_from_edit_page(0)
-    assert contact_from_home_page.homephone == clear(contact_from_edit_page.homephone)
-    assert contact_from_home_page.workphone == clear(contact_from_edit_page.workphone)
-    assert contact_from_home_page.mobilephone == clear(contact_from_edit_page.mobilephone)
-    assert contact_from_home_page.secondaryphone == clear(contact_from_edit_page.secondaryphone)
+    assert contact_from_home_page.all_phones_from_home_page == merge_phones_like_on_home_page(contact_from_edit_page)
+
 
 def test_phones_on_contact_view_page(app):
     contact_from_view_page = app.contact.get_contact_from_view_page(0)
@@ -22,4 +21,8 @@ def test_phones_on_contact_view_page(app):
 def clear(s):
     return re.sub("[() -]", "", s)
 
-
+# Важно как происходит мерж строки! Возникла проблема, что сравниваются разные параметры
+def merge_phones_like_on_home_page(contact):
+    return "\n".join(filter(lambda x: x != "",
+                            map(lambda x: clear(x),
+                                [contact.homephone, contact.mobilephone, contact.workphone, contact.secondaryphone])))
