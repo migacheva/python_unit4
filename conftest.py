f2ba46ebc3d3fdc43eb0377d152b10c486ac9d99
migacheva <migacheva9>
2020-02-16 23:54:38 +0300

    Передача параметров из командной строки

--- conftest.py
+++ conftest.py
@@ -4,21 +4,33 @@ from fixture.application import Application
 
 fixture = None
 
+
 @pytest.fixture
 def app(request):
-  global fixture
-  if fixture is None:
-    fixture = Application()
-  else:
-    if not fixture.is_valid():
-      fixture = Application()
-  fixture.session.ensure_login(username="admin", password="secret")
-  return fixture
+    global fixture
+    browser = request.config.getoption("--browser")
+    base_url= request.config.getoption("--baseURL")
+    # home_url= request.config.getoption("--homeURL")
+    if fixture is None:
+        fixture = Application(browser=browser, base_url=base_url)
+    else:
+        if not fixture.is_valid():
+            fixture = Application(browser=browser, base_url=base_url)
+    fixture.session.ensure_login(username="admin", password="secret")
+    return fixture
+
 
 @pytest.fixture(scope="session", autouse=True)
 def stop(request):
-  def fin():
-    fixture.session.ensure_logout()
-    fixture.destoy()
-  request.addfinalizer(fin)
-  return fixture
+    def fin():
+        fixture.session.ensure_logout()
+        fixture.destoy()
+
+    request.addfinalizer(fin)
+    return fixture
+
+
+def pytest_addoption(parser):
+    parser.addoption("--browser", action="store", default="chrome")
+    parser.addoption("--baseURL", action="store", default="http://localhost:8080/addressbook/")
+    parser.addoption("--homeURL", action="store", default="http://localhost:8080/addressbook/")
