bcc5aecf38ee512c95348ff88b70b8f37f87a9c9
Anna Kovalchuk <anutamig@mail.ru>
2020-02-17 18:58:55 +0300

    Добавление конф файла

--- conftest.py
+++ conftest.py
@@ -1,22 +1,22 @@
 # -*- coding: utf-8 -*-
 import pytest
+import json
 from fixture.application import Application
 
 fixture = None
-
+target = None
 
 @pytest.fixture
 def app(request):
     global fixture
+    global target
     browser = request.config.getoption("--browser")
-    base_url= request.config.getoption("--baseURL")
-    # home_url= request.config.getoption("--homeURL")
-    if fixture is None:
-        fixture = Application(browser=browser, base_url=base_url)
-    else:
-        if not fixture.is_valid():
-            fixture = Application(browser=browser, base_url=base_url)
-    fixture.session.ensure_login(username="admin", password="secret")
+    if target is None:
+        with open(request.config.getoption("--target")) as config_file:
+            target = json.load(config_file)
+    if fixture is None or not fixture.is_valid():
+        fixture = Application(browser=browser, base_url=target["baseURL"])
+    fixture.session.ensure_login(username=target["username"], password=target["password"])
     return fixture
 
 
@@ -32,5 +32,5 @@ def stop(request):
 
 def pytest_addoption(parser):
     parser.addoption("--browser", action="store", default="chrome")
-    parser.addoption("--baseURL", action="store", default="http://localhost:8080/addressbook/")
-    parser.addoption("--homeURL", action="store", default="http://localhost:8080/addressbook/")
+    # parser.addoption("--baseURL", action="store", default="http://localhost:8080/addressbook/")
+    parser.addoption("--target", action="store", default="target.json")
